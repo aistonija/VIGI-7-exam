84d094797c2fc0798bbff8b57e16729b90874f87
Aiste Zuperkaite <64789259+aistonija@users.noreply.github.com>
2021-10-27 18:44:29 +0300

    Task 9 updated English

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,12 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Create class constructor "Movie" (use ES6 syntax), which will be able to create objects with properties and 1 method:
+
+Properties:
+title, director, budget
+Method: 
+wasExpensive() - if movie budget is more than 100 000 000 mln USD, method will return true, otherwise - false.
+------------------------------------------------------------------------------------------------------ */
