dcb964fd1a9906038477f9c93d75a9490c5fda66
Aiste Zuperkaite <64789259+aistonija@users.noreply.github.com>
2021-10-27 18:32:22 +0300

    Task 4 updated English

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,12 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
+/* ------------------------------ TASK 4 ENGLISH -----------------------------------
+Write JS code, where all information about the cars will be printed from file "cars.json" (endpoint and file provided).
+
+Each brand should have their own "card", where brand name and models should be printed.
+
+Note: Card about the brand and models, must have minimum style.
+-------------------------------------------------------------------------- */
+
 const ENDPOINT = "cars.json";
