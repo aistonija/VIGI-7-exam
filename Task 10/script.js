160786fbff6cea35ed4255a5b3d8b4457fcb3b68
Aiste Zuperkaite <64789259+aistonija@users.noreply.github.com>
2021-10-27 18:49:18 +0300

    Task 10 updated English

--- Task 10/script.js
+++ Task 10/script.js
@@ -11,3 +11,16 @@ Jeigu įvesta vertė tiko - nusispalvina to skaičiaus langelis lentelėje.
 
 Jeigu norite, galite naudotis turimu CSS, bet galite pasirašyti ir savo. 
 -------------------------------------------------------------------------- */
+
+/* ------------------------------ TASK 10 -----------------------------------
+Write JS code, where the user will be able to enter number into form. Number cannot be less than 1 or more than 9. (1-9 included).
+
+If wrong number was entered - error message appears, which will ask to enter correct number. 
+Error message must be printed in <div class="error"> block.
+
+If correct number was entered - table cell with that number must be colored (background color).
+
+Once new number is entered - previous table cell changes background-color to white, and new number table cell bg changes. 
+
+Feel free to use the html and css code provided, but you can also write your own. 
+-------------------------------------------------------------------------- */
