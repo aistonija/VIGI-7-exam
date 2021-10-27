f6cb1f5db95e18a94bb6b8806df7c45fc0aa6606
Aiste Zuperkaite <64789259+aistonija@users.noreply.github.com>
2021-10-27 18:23:54 +0300

    Task 1 & 2 in English

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,12 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+/* ------------------------------ TASK 1 ENGLISH ----------------------------
+Write JS code, where user will be able to enter the weigth in kilograms (kg) and see the converted results into:
+1. Pounds (lb) / formula: lb = kg * 2.2046;
+2. Grams (g) / formula: g = kg / 0.0010000;
+3. Ounces (oz) / formula: oz = kg * 35.274
+
+Note: Results must be printed on form submit in <div id="output"></div> code block, also it must have minimum style.
+------------------------------------------------------------------- */
--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,7 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+/* ------------------------------ TASK 2 ENGLISH ----------------------------
+Write JS code, which will count how many times the button with text "CLICK ME" was clicked. Result of the clicks must be seen on the right side counting state block (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
