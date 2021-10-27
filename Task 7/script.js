ae244e12b7d4d6e07887cc5c699e748eb4a74440
Aiste Zuperkaite <64789259+aistonija@users.noreply.github.com>
2021-10-27 18:39:15 +0300

    Task 7 updated English

--- Task 7/script.js
+++ Task 7/script.js
@@ -4,6 +4,12 @@ Turimas "audi" objektas.
 Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objektą 
 ir grąžins visus jo "keys" masyve.
 -------------------------------------------------------------------------- */
+
+/* ------------------------------ TASK 7 -----------------------------------
+You'll find an object of car below
+
+Write function "showObjectKeys", as an argument will receive an object, and will return all of its "keys" in new array.
+-------------------------------------------------------------------------- */
 const audi = {
   make: "audi",
   model: "A6",
